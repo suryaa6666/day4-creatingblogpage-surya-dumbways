8f91fe3e6d07e85353fe2914d53fcb61f6cbdd6c
suryaa6666 <suryaelidanto@gmail.com>
2022-06-10 23:19:08 +0700

    myproject-detail

---
+++ myproject.js
@@ -0,0 +1,26 @@
+// let dataBlog = [];
+
+// function addBlog(event) {
+//     event.preventDefault();
+
+//     let title = document.getElementById('input-blog-title').value;
+//     let content = document.getElementById('input-blog-content').value;
+//     let image = document.getElementById('input-blog-image').files;
+
+//     image = URL.createObjectURL(image[0]);
+
+//     dataBlog.push({
+//         title,
+//         content,
+//         image
+//     });
+
+//     console.log(dataBlog);
+
+//     renderPost();
+// }
+
+// function renderPost() {
+//     document.getElementById('contents').innerHTML = '';
+// }
+
